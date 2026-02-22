6cc14d02225d2536f6bddff54722f5d2993b8065
Kisahy <kimsanghyeon.kr@gmail.com>
2026-02-22 19:19:48 +0900

    chore: initialize product-service project

---
+++ src/main/java/com/kisahy/commerce/product/CommerceProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kisahy.commerce.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kisahy/commerce/product/CommerceProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kisahy.commerce.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
